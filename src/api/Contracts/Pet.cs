941d46c169d177c9e7a10629889df5be934eef21
WrinLangford <82046357+WrinLangford@users.noreply.github.com>
2022-06-11 18:06:47 +0100

    Nulalble fields and launchsettings fix

--- src/api/Contracts/Pet.cs
+++ src/api/Contracts/Pet.cs
@@ -5,9 +5,9 @@ public class Pet
     public long Id { get; set; }
     public long OwnerId { get; set; }
     public long TypeId { get; set; }
-    public string Breed { get; set; }
+    public string? Breed { get; set; }
     public string Name { get; set; }
-    public string Bio { get; set; }
+    public string? Bio { get; set; }
     public string? PictureUrl { get; set; }
     public int ListOrder { get; set; }
 }
--- src/api/Contracts/User.cs
+++ src/api/Contracts/User.cs
@@ -7,8 +7,8 @@ public class User
     public bool PetOwner { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string PreferredName { get; set; }
-    public string Bio { get; set; }
+    public string? PreferredName { get; set; }
+    public string? Bio { get; set; }
     public string? PictureUrl { get; set; }
     public string Email { get; set; }
 }
